3e4ff410536dbbc139be0b9dea6c941edd256799
Marcelo Higuti <marcelohiguti96@gmail.com>
2024-02-10 11:15:04 -0300

    init events timeline project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Timeline } from "./components/Timeline"
+
+function App() {
+  return (
+    <Timeline />
+  )
+}
+
+export default App
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+export const Timeline: FC = () => {
+  return (
+    <h1>timeline app</h1>
+  )
+}
